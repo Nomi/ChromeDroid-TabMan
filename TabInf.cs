1a7fda46ce6278109ed3ea0a8f4a7d9a9031dce9
Noman <baba.noman@live.com>
2022-02-15 17:26:03 +0100

    Revert "*Editied TabInf class:*"
    This reverts commit 487f61434c136a65a47cd246b8a8062f6919dcb3.

--- TabInf.cs
+++ TabInf.cs
@@ -11,32 +11,24 @@ namespace ChromeDroid_TabMan
     class TabInf: IComparable<TabInf>
     {
         [JsonIgnore]
-        public int tabPosition=-1;
+        public readonly int tabPosition;
         [JsonPropertyName("url")]
-        public string url
-        {
-            get { return url; }
-            set { url = value; baseWebsite = GetBasewebsite(); }
-        }
+        public string url;
         [JsonIgnore]
-        public string title;
+        public readonly string title;
         [JsonIgnore]
-        public string baseWebsite { get; private set; }
+        public readonly string baseWebsite;
         [JsonPropertyName("title")]
         public string lastKnownTitle;
 
-        public TabInf()
+        public TabInf(string url, string lkTitle)
         {
-
+            this.url = url;
+            this.title = "title-to-be-implemented";
+            this.lastKnownTitle = lkTitle;
+            this.tabPosition = -1;
+            baseWebsite = GetBasewebsite();
         }
-        //public TabInf(string url, string lkTitle)
-        //{
-        //    this.url = url;
-        //    this.title = "title-to-be-implemented";
-        //    this.lastKnownTitle = lkTitle;
-        //    this.tabPosition = -1;
-        ////    baseWebsite = GetBasewebsite();//Not needed after the set method i made for title
-        //}
         public TabInf(string url, string title, int tabPos)
         {
             this.url = url;
